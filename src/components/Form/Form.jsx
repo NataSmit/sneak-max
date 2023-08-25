54dade57f850dbffda31d3ea9512298283b5381f
Natalia Gorobtsova <gorobzova_natalia@mail.ru>
2023-08-26 01:08:05 +0300

    sizes and checkboxes added

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,50 +1,98 @@
-import React, { useState } from 'react'
-import { Box, Typography, Slider, TextField } from '@mui/material'
+import React, { useState } from "react";
+import {
+  Box,
+  Typography,
+  Slider,
+  TextField,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import styles from './Form.module.scss'
+import Button from '../../ui/Button/Button'
+
+const sizes = [35, 36, 37, 38, 39, 40, 41, 42, 43]
 
 export default function Form() {
-  const [valueMin, setValueMin] = useState(0)
-  const [valueMax, setValueMax] = useState(100000)
+  const [valueMin, setValueMin] = useState(0);
+  const [valueMax, setValueMax] = useState(100000);
   const handleSliderChange = (event, newValue) => {
     setValueMin(newValue[0]);
-    setValueMax(newValue[1])
+    setValueMax(newValue[1]);
   };
 
   function handleMinInputChange(e) {
-    setValueMin(e.target.value)
+    setValueMin(e.target.value);
   }
 
   function handleMaxInputChange(e) {
-    setValueMax(e.target.value)
+    setValueMax(e.target.value);
   }
 
   return (
-    <Box component='form' sx={{width: 280, backgroundColor: 'rgba(255, 244, 238, 1)', padding: 2}} >
-      <Typography 
-      component='p' variant='h5' 
-      color='rgba(68, 75, 88, 1)'
-      my={2}
-      >
+    <Box
+      component="form"
+      sx={{ width: 280, backgroundColor: "rgba(255, 244, 238, 1)", padding: 2 }}
+    >
+      <Typography component="p" variant="h5" color="rgba(68, 75, 88, 1)" my={2}>
         Подбор по параметрам
       </Typography>
-      <Box >
-        <Typography my={1} color='rgba(68, 75, 88, 1)'>Цена, руб</Typography>
-        <TextField variant="outlined" sx={{width: 135}} value={valueMin} onChange={handleMinInputChange} />
-        <TextField variant="outlined" sx={{width: 135}} value={valueMax} onChange={handleMaxInputChange} />
+      <Box>
+        <Typography my={1} color="rgba(68, 75, 88, 1)">
+          Цена, руб
+        </Typography>
+        <TextField
+          variant="outlined"
+          sx={{ width: 135 }}
+          value={valueMin}
+          onChange={handleMinInputChange}
+        />
+        <TextField
+          variant="outlined"
+          sx={{ width: 135 }}
+          value={valueMax}
+          onChange={handleMaxInputChange}
+        />
       </Box>
-      <Slider 
+      <Slider
         value={[valueMin, valueMax]}
         onChange={handleSliderChange}
         valueLabelDisplay="auto"
         min={0}
         max={100000}
         sx={{
-          color: 'rgba(68, 75, 88, 1)',
-          '& .MuiSlider-thumb': {
-            borderRadius: '0',
+          color: "rgba(68, 75, 88, 1)",
+          "& .MuiSlider-thumb": {
+            borderRadius: "0",
             width: 4,
           },
         }}
-        />
+      />
+      <Box>
+        <Typography my={1} color="rgba(68, 75, 88, 1)">
+          Пол
+        </Typography>
+        <FormGroup row>
+          <FormControlLabel
+            control={
+              <Checkbox/>
+            }
+            label="Мужской"
+          />
+          <FormControlLabel control={<Checkbox />} label="Женский" />
+        </FormGroup>
+      </Box>
+      <Box>
+        <Typography my={1} color="rgba(68, 75, 88, 1)">
+          Размер
+        </Typography>
+        <div className={styles.sizes}>
+          {sizes.map((size) => (
+            <div className={styles.size}>{size}</div>
+          ))}
+        </div>
+      </Box>
+      <Button text={'Применить'}/>
     </Box>
-  )
+  );
 }
